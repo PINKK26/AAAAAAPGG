cea500815b16baa4dea1ae069a53e423d6557219
Isabella Simonetti <128994065+PINKK26@users.noreply.github.com>
2025-11-08 20:22:50 -0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,4 @@
+// supabase.js
+const supabaseUrl = 'https://jennxlgbaqlneuxunhqw.supabase.co'; // reemplaza por tu URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Implbm54bGdiYXFsbmV1eHVuaHF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNjQ4ODMsImV4cCI6MjA3Nzk0MDg4M30.2iMvCkcJR4lrkwee9tK8wpyQYZs2Otx0VQNKd1sqXgk'; // reemplaza por tu key pública
+const supabase = supabase.createClient(supabaseUrl, supabaseKey);
